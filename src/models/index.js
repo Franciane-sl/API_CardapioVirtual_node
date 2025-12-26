bdaa22e877f05d571569a70170d9374f370b7cea
Franciane Sl <franciane.leitee@gmail.com>
2025-12-26 18:47:15 -0300

    feat: add User and Adress models with 1:1 relation"

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,7 @@
 const Category = require('./Category');
 const Product = require('./Product');
+const User = require('./User');
+const Adress = require ('./Adress')
 
 
 Category.hasMany(Product, {
@@ -13,7 +15,19 @@ Product.belongsTo(Category, {
   as: 'category'
 });
 
+User.hasOne(Adress,{
+  foreignKey:'user_id',
+  as: 'adress'
+})
+
+Adress.belongsTo(User,{
+  foreignKey:'user_id',
+  as: 'user'
+})
+
 module.exports = {
   Category,
-  Product
+  Product,
+  User,
+  Adress
 };
