c901f55642c99bbd19caef90f7236ebde791ac55
Franciane Sl <franciane.leitee@gmail.com>
2025-12-25 21:27:57 -0300

    feat: add category model

---
+++ src/models/Category.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Category = sequelize.define('Category', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  descricao: {
+    type: DataTypes.STRING,
+    allowNull: true
+  }
+}, {
+  tableName: 'categories',
+  timestamps: true
+});
+
+module.exports = Category;
