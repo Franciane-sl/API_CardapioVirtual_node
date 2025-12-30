63b6630305d2616cd1a1e9773f543b673fac5489
Franciane Sl <franciane.leitee@gmail.com>
2025-12-30 19:40:52 -0300

    feat: register Product routes in app.js

--- src/app.js
+++ src/app.js
@@ -3,13 +3,15 @@ const express = require('express');
 require('./models')
 
 const errorHandler = require('./middleware/errorHandler');
-const categoryRoutes = require('./routes/CategoryRoutes')
+const categoryRoutes = require('./routes/CategoryRoutes');
+const productRoutes = require('./routes/ProductRoutes')
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/categories', categoryRoutes)
+app.use('/categories', categoryRoutes);
+app.use('/products', productRoutes);
 
 app.use(errorHandler);
 
