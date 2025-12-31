ef71a9c675f5a81cd96244fd0ca2457ddb1db911
Franciane Sl <franciane.leitee@gmail.com>
2025-12-31 20:19:45 -0300

    feat: register User routes in app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ require('./models')
 
 const errorHandler = require('./middleware/errorHandler');
 const categoryRoutes = require('./routes/CategoryRoutes');
-const productRoutes = require('./routes/ProductRoutes')
+const productRoutes = require('./routes/ProductRoutes');
+const userRoutes = require('./routes/UserRoutes')
+
 
 const app = express();
 
@@ -12,6 +14,7 @@ app.use(express.json());
 
 app.use('/categories', categoryRoutes);
 app.use('/products', productRoutes);
+app.use('/users', userRoutes);
 
 app.use(errorHandler);
 
